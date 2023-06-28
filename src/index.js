1257985e526137811b630226a812689c4f144ebb
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-06-28 13:00:33 +0700

    Implement PropTypes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
 // import "./index.css";
 // import App from "./App";
 
+const Test = () => {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating
+        maxRating={10}
+        color="green"
+        onSetRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarRating maxRating={5} />
+    <StarRating size={24} color="red" maxRating={10} />
+    <Test />
   </React.StrictMode>
 );
