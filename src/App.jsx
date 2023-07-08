e6f35af6326feb1a0a8dc6dc764a69b98d5bff5b
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-07-08 14:42:31 +0700

    Learn how the hooks work in React

--- src/App.jsx
+++ src/App.jsx
@@ -246,8 +246,8 @@ const MovieDetails = ({
     Genre: genre,
   } = movie;
 
-  /* eslint-disable */
   // if (imdbRating > 8) [isTop, setIsTop] = useState(true);
+  // if (imdbRating > 8) return <p>Greatest ever</p>;
 
   const handleAdd = () => {
     const newWatchedMovie = {
