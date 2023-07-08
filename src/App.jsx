169dab4784cc3211cd8f1c2b19f4c6abc3e933ce
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-07-08 17:42:54 +0700

    Add localstorage to save the state of the app

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,15 @@ const KEY = "963ef6a7";
 const App = () => {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(() => {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   const handleSelectMovie = (id) => {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -24,6 +29,11 @@ const App = () => {
 
   const handleAddWatched = (movie) => {
     setWatched((watched) => [...watched, movie]);
+
+    // localStorage.setItem(
+    //   "watched",
+    //   JSON.stringify([...watched, movie])
+    // );
   };
 
   const handleDeleteWatched = (id) => {
@@ -31,6 +41,10 @@ const App = () => {
       watched.filter((movie) => movie.imdbID !== id)
     );
   };
+
+  useEffect(() => {
+    localStorage.setItem("watched", JSON.stringify(watched));
+  }, [watched]);
 
   useEffect(() => {
     const controller = new AbortController();
